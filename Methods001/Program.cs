be38b67068f6756a09087664cc06a5c55570cc87
Tatyana <kta995@mail.ru>
2022-12-01 20:23:16 +0300

    Methods001

---
+++ Methods001/Program.cs
@@ -0,0 +1,57 @@
+﻿//Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор...."); // ничего не возвращает, ничего не принимает. Будет показывать на экран
+}
+
+
+//Method1(); //как вызывается этот метод
+
+//Вид 2
+
+void Method2(string msg)  //ничего не возвращают, но могут принимать аргументы
+{
+    Console.WriteLine(msg);
+
+}
+//Method2(msg: "Текст собщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++; //инкремент
+    }
+}
+// Method21(msg: "Текст", count: 4);
+// Method21( count: 4, msg: "Текст");
+
+// Вид3
+
+int Method3()   //что-то возвращают, но ничего не принимают//сначала нужно указать тип данных значение которых мы ожидаем
+{
+    return DateTime.Now.Year;
+}
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+//Вид 4 //что-то возвращают и что-то принимают
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+
+}
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
